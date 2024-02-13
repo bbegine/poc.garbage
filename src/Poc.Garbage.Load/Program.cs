5a1a4ae9c6fa4077005db76fb1eddfcc02fa33ac
Bob Begine <24271446+bbegine@users.noreply.github.com>
2024-02-13 11:49:35 +0100

    Initital

---
+++ src/Poc.Garbage.Load/Program.cs
@@ -0,0 +1,47 @@
+﻿using NBomber.CSharp;
+using NBomber.Http.CSharp;
+
+string workstationApi = @"https://pocgarbageworkstationapi20240212094046.azurewebsites.net/";
+string serverApi = @"https://pocgarbageserverapi20240212085220.azurewebsites.net/";
+
+using var httpClient = new HttpClient();
+
+var wsScenario = Scenario.Create("workstation_gc_scenario", async context =>
+{
+    var request =
+        Http.CreateRequest("GET", $"{workstationApi}gc");
+    var response = await Http.Send(httpClient, request);
+
+    return response;
+})
+.WithoutWarmUp()
+.WithLoadSimulations(
+    Simulation.Inject(rate: 3,
+                      interval: TimeSpan.FromSeconds(1),
+                      during: TimeSpan.FromMinutes(10))
+);
+
+var serverScenario = Scenario.Create("server_gc_scenario", async context =>
+{
+    var request =
+    Http.CreateRequest("GET", $"{serverApi}gc");
+        //.WithHeader("Accept", "text/html");
+    // .WithHeader("Accept", "application/json")
+    // .WithBody(new StringContent("{ id: 1 }", Encoding.UTF8, "application/json");
+    // .WithBody(new ByteArrayContent(new [] {1,2,3}))
+
+
+    var response = await Http.Send(httpClient, request);
+
+    return response;
+})
+.WithoutWarmUp()
+.WithLoadSimulations(
+    Simulation.Inject(rate: 3,
+                      interval: TimeSpan.FromSeconds(1),
+                      during: TimeSpan.FromMinutes(10))
+);
+
+NBomberRunner
+    .RegisterScenarios(wsScenario, serverScenario)
+    .Run();
---
+++ src/Poc.Garbage.WorkstationApi/Program.cs
@@ -0,0 +1,68 @@
+using System.Runtime;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/gc", () =>
+{
+    var str = new string('R', 100000000);
+    return new
+    {
+        FragmentedBytes = GC.GetGCMemoryInfo().FragmentedBytes,
+        GarbageCollection = GCSettings.IsServerGC ? "Server" : "Workstation"
+    };
+})
+.WithName("GC")
+.WithOpenApi();
+
+app.MapGet("/collect", () =>
+{
+    GC.Collect();
+    return new { Status = "Ok"};
+})
+.WithName("collect")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
